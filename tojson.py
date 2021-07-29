fc33711fa4b26a94abed603d0452d9b07379b608
lima <lima@mbp>
2021-07-29 16:23:15 -0500

    updates

--- tojson.py
+++ tojson.py
@@ -107,7 +107,7 @@ re_list = re.compile(r"(\S+) {(?:([^{}]*))}")
 store_contex = {"ltm:virtual": ["stuff"]}
 
 list_keys = [
-    # "rules",
+    "rules",
     "images",
     "variables",
     "rows",
